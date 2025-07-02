c120b1d0c7f3b9427c384407ee24d0b91304bec9
BunnyBun <kethushanmukhareddy2126@gmail.com>
2025-07-02 13:54:38 +0530

    Src: setup Game.h and main.cpp

---
+++ src/Game.h
@@ -0,0 +1,53 @@
+#pragma once
+
+
+#include"EntityManager.hpp"
+#include<SFML/Graphics.hpp>
+
+#include"imgui.h"
+#include"imgui-SFML.h"
+
+struct PlayerConfig {int SR, CR, FR, FG, FB, OR, OG, OB, OT, V; float S; };
+struct EnemyConfig {int SR, CR, OR, OG, OB, OT, VMIN, VMAX, L, SI ; float SMAX, SMIN; };
+struct BulletConfig {int SR, CR, FR, FG, FB, OR, OG, OB, OT, V, L; float S; };
+
+class Game {
+    sf::RenderWindow m_window;    //the window we will draw
+    EntityManager    m_entities;  //vectors of enities to maintain
+    sf::Font         m_font;      //font we will use to draw
+    sf::Text         m_text;      //the score text to be drawn to the screen
+    PlayerConfig     m_playerConfig;
+    EnemyConfig      m_enemyConfig;
+    sf::Clock        m_deltaClock;
+    int              m_score=0;
+    int              m_currentFrame=0;
+    int              m_lastEnenmySpawnTime=0;
+    bool             m_paused=false;
+    bool             m_running=true;
+    
+    void init(const std::string &config); //initialize game from a config file
+    void setPaused(bool paused);
+
+    void spawnPlayer();
+    void spawnEnemy();
+    void spawnSmallEnemies(std::shared_ptr<Entity> e);
+    void spawnBullet(std::shared_ptr<Entity> entity, const Vec2f& target);
+    void spawnSpecialWeapon(std::shared_ptr<Entity> entity);
+
+    void sMovement();
+    void sLifespan();
+    void sCollision();
+    void sEnemySpawner();
+    void sGUI();
+    void sRender();
+    void sUserInput();
+
+    std::shared_ptr<Entity> player();
+
+    public:
+
+    Game(const std::string& connfig);
+
+    void run();
+
+};
